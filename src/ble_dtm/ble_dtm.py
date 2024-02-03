f830723336e9b25ee994d58cd98d498acf3f8925
Tom Benezech <tom.leglaunec@gmail.com>
2024-02-03 18:17:25 +0100

    refactor: set max line length to 127

--- src/ble_dtm/ble_dtm.py
+++ src/ble_dtm/ble_dtm.py
@@ -30,7 +30,9 @@ class Status(IntEnum):
 
 
 class DirectTestMode:
-    """Initialize Direct Test Mode over serial. Either a `serial.Serial` instance or a port and options should be provided. Passing both ser and port will raise a TypeError. Serial options are unused if a custom serial instance is provided."""
+    """Initialize Direct Test Mode over serial. Either a `serial.Serial` instance or a port and options should be provided.
+    Passing both ser and port will raise a TypeError. Serial options are unused if a custom serial instance is provided.
+    """
 
     def __init__(
         self,
@@ -61,50 +63,45 @@ class DirectTestMode:
         """Resets DUT by sending a Le_Test_Setup command with control and parameter set to 0x0."""
         self._write_setup(Command.LE_TEST_SETUP, 0, 0)
         return cast(LE_Test_Status, self._read())
-    
+
     def stop_test(self) -> LE_Packet_Report:
-        """Stops current test by sending a LE_Test_End command. Returns Packet report answered by DUT."""
+        """Stops current test by sending a LE_Test_End command. Returns Packet report answered by
+        DUT."""
         # Core Specification permits Parameter from value 0x00 to 0x03 but doesn't explicitly specify behavior (no effect?).
         # This implementation will only send Parameter of value 0.
         self._write_setup(Command.LE_TEST_END, 0, 0)
         return cast(LE_Packet_Report, self._read())
 
-    def start_transmitter_test(
-        self, frequency: int, length: int, pkt: PacketType
-    ) -> LE_Test_Status:
-        """Starts transmitter test by sending a LE_Transmitter_Test command.
+    def start_transmitter_test(self, frequency: int, length: int, pkt: PacketType) -> LE_Test_Status:
+        """Starts receiver test by sending a LE_Receiver_Test command.
         Parameters:
             frequency: channel N selection from 0 to 39; (2N + 2402) MHz (No correspondance with LE channels denomiation!)
-            length: length of packet payload in bits (limited to 63, if longer packet are needed see the setup command, for up to 255)
-            pkt: packet type. `PacketType.VENDOR_SPECIFIC` will result in vendor specific packet type for LE Uncoded PHYs and 11111111 for LE Coded PHY.
+            length: length of packet payload in bits (limited to 63, if longer packet are needed see the setup command,
+            for up to 255)
+            pkt: packet type. `PacketType.VENDOR_SPECIFIC` will result in vendor specific packet type for LE Uncoded PHYs and
+            11111111 for LE Coded PHY.
         """
         self._write_test(Command.LE_TRANSMITTER_TEST, frequency, length, pkt)
         return cast(LE_Test_Status, self._read())
 
-    def start_receiver_test(
-        self, frequency: int, length: int, pkt: PacketType
-    ) -> LE_Test_Status:
+    def start_receiver_test(self, frequency: int, length: int, pkt: PacketType) -> LE_Test_Status:
         """Starts receiver test by sending a LE_Receiver_Test command.
         Parameters:
             frequency: channel N selection from 0 to 39; (2N + 2402) MHz (No correspondance with LE channels denomiation!)
-            length: length of packet payload in bits (limited to 63, if longer packet are needed see the setup command, for up to 255)
-            pkt: packet type. `PacketType.VENDOR_SPECIFIC` will result in vendor specific packet type for LE Uncoded PHYs and 11111111 for LE Coded PHY.
+            length: length of packet payload in bits (limited to 63, if longer packet are needed see the setup command,
+            for up to 255)
+            pkt: packet type. `PacketType.VENDOR_SPECIFIC` will result in vendor specific packet type for LE Uncoded PHYs and
+            11111111 for LE Coded PHY.
         """
         self._write_test(Command.LE_RECEIVER_TEST, frequency, length, pkt)
         return cast(LE_Test_Status, self._read())
 
-    def _write_test(
-        self, cmd: int, frequency: int, length: int, pkt: PacketType
-    ) -> None:
+    def _write_test(self, cmd: int, frequency: int, length: int, pkt: PacketType) -> None:
         if frequency < 0 or frequency > 0x27:
-            raise ReservedForFutureUseError(
-                "Argument frequency must be in range 0 to 39."
-            )
+            raise ReservedForFutureUseError("Argument frequency must be in range 0 to 39.")
 
         if length < 0 or length > 0x3F:
-            raise ValueError(
-                "Argument length must be in range 0 to 63 (0x3F). See documentation for bigger payloads up to 255 bits."
-            )
+            raise ValueError("Argument length must be in range 0 to 63 (0x3F).")
 
         if not isinstance(pkt, PacketType):
             raise TypeError("Argument pkt must be of type PacketType.")
@@ -116,7 +113,7 @@ class DirectTestMode:
 
     def _write_setup(self, cmd: int, control: int, parameter: int) -> None:
         # See paragraph 3.3.2 of Bluetooth Core v5.4, Vol 6, Part F.
-        message = ((cmd << 14) + (control << 8) + parameter)
+        message = (cmd << 14) + (control << 8) + parameter
         n = self._serial.write(message.to_bytes(2))
         if not n == 2:
             raise RuntimeWarning(f"{n} byte(s) were sent, expected 2.")
@@ -126,21 +123,21 @@ class DirectTestMode:
 
 
 class DUTResponse(ABC):
-    """Represents the DUT response. A response (LE_Status or LE_Packet_Report) consist of 2 bytes."""
+    """Represents the DUT response.
+    A response (LE_Status or LE_Packet_Report) consist of 2 bytes.
+    """
 
     _event: Event
 
-    @classmethod
-    def from_raw(cls, raw: bytes) -> DUTResponse:
-        """Factory method that returns a new instance of a DUTResponse child class, depending on the event type.
-        The
+    @staticmethod
+    def from_raw(raw: bytes) -> DUTResponse:
+        """Factory method that returns a new instance of a DUTResponse child class,
+        depending on the event type.
         """
         if not isinstance(raw, bytes):
-            raise TypeError(
-                f"Arg raw must be of type bytes, {raw.__class__.__name__} passed."
-            )
-        if not (l := len(raw)) == 2:
-            raise ValueError(f"Arg raw should be of length 2 (bytes), {l} instead.")
+            raise TypeError(f"Arg raw must be of type bytes, {raw.__class__.__name__} passed.")
+        if not (length := len(raw)) == 2:
+            raise ValueError(f"Arg raw should be of length 2 (bytes), {length} instead.")
 
         packet = int.from_bytes(raw)
 
@@ -155,7 +152,6 @@ class DUTResponse(ABC):
 
 
 class LE_Test_Status(DUTResponse):
-
     _st: Status
 
     def __init__(self, response: int) -> None:
@@ -169,13 +165,13 @@ class LE_Test_Status(DUTResponse):
 
     def decode_setup(self, ctrl: int):
         """Decode Response field contained in LE_Test_Status event.
-        As to Core Specification V5.4, this field has a meaning only in response to a LE_Test_Setup command with control parameters `0x05` and `0x09`.
-        Raise ReservedForFutureUseError for other control parameters code, or when status is Error, as the response field is Reserved for future use.
+        As to Core Specification V5.4, this field has a meaning only in response to a LE_Test_Setup command with control
+        parameters `0x05` and `0x09`.
+        Raise ReservedForFutureUseError for other control parameters code, or when status is Error, as the response field is
+        Reserved for future use.
         """
         if self.status == Status.ERROR:
-            raise ReservedForFutureUseError(
-                "Nothing to decode in Error status, Reserved for future use."
-            )
+            raise ReservedForFutureUseError("Nothing to decode in Error status, Reserved for future use.")
 
         if ctrl == 0x05:
             return ...
@@ -183,13 +179,10 @@ class LE_Test_Status(DUTResponse):
         if ctrl == 0x09:
             return ...
 
-        raise ReservedForFutureUseError(
-            f"Invalid control parameter code: {hex(ctrl)}. Please check function documentation."
-        )
+        raise ReservedForFutureUseError(f"Invalid control parameter code: {hex(ctrl)}." "Please check function documentation.")
 
 
 class LE_Packet_Report(DUTResponse):
-
     _packet_count: int
 
     def __init__(self, response: int) -> None:
@@ -199,7 +192,8 @@ class LE_Packet_Report(DUTResponse):
     @property
     def packet_count(self) -> int:
         """Returns the number of packets received by the DUT (buffer of 15 bits, so up to 32767).
-        Note: The DUT is not responsible for any overflow conditions of the packet count. That responsibility belongs with the RFPHY Tester or other auxiliary equipment.
+        Note: The DUT is not responsible for any overflow conditions of the packet count. That responsibility belongs with the
+        RFPHY Tester or other auxiliary equipment.
         """
         return self._packet_count
 
